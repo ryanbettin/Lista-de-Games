ecc61f6079635c3a73449c600f01c65a63a21e39
ryanbettin <ryanbettin44@gmail.com>
2025-08-24 14:43:52 -0300

    projeto criado

---
+++ src/main/java/com/ryanbettin/ListGames/ListGamesApplication.java
@@ -0,0 +1,13 @@
+package com.ryanbettin.ListGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanbettin/ListGames/ListGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanbettin.ListGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
